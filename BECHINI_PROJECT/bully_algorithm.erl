1f534ec9716af25b447484491424662825cf120f
zaccaria97 <51884612+zaccaria97@users.noreply.github.com>
2021-06-21 22:08:27 +0200

    Update bully_algorithm.erl

--- BECHINI_PROJECT/bully_algorithm.erl
+++ BECHINI_PROJECT/bully_algorithm.erl
@@ -38,7 +38,7 @@
 -define(SERVER,?MODULE).
 -include("utility.hrl").
 
--export([start_link/1, start_election_process/1, init/1, terminate/2, handle_info/2, handle_call/3, handle_cast/2]).
+-export([start_link/1, start_election_process/0, start_election_process/1, init/1, terminate/2, handle_info/2, handle_call/3, handle_cast/2]).
 
 
 start_link(Neighbours) ->
@@ -55,6 +55,11 @@ init(Neighbours) ->
 	% The function is expected to return {ok,State} where State is the internal state of the gen_server
 	{ok,{Neighbours,no_leader}}.
 
+start_election_process() ->
+	% Stops the process that manages the normal behaviour of the secondary node
+	gen_server:call({secondary_node, node()}, stop, ?CALL_TIMEOUT),
+	spawn(primary_node, elect, []).
+	
 start_election_process(Neighbours) ->
 	% Stops the process that manages the normal behaviour of the secondary node
 	gen_server:call({secondary_node, node()}, stop, ?CALL_TIMEOUT),
@@ -65,28 +70,22 @@ start_election_process(Neighbours) ->
 		Result_atom == ok ->
 			start_election(Neighbours);
 		true ->
-			io:format("~n ...An error occurred, node will shut down... ~n"),
+			io:format("~n ...An error occurred, node will shut down... ~n")
 			% (?) Come si termina il processo corrente
 	end.
 
+	
 start_election(Neighbours_list) ->
+
 	Higher_id_list = get_neighbours_with_higher_id(Neighbours_list),
+	io:format("@@@@@Higher_id_list ~w~n",[Higher_id_list]), % DEBUG
 	if
 		Higher_id_list == [] ->
-			% (1)  Case the current node is the winner
-			broadcast_leader(Neighbours_list),
-			spawn(primary_node, elect, []);
+			% (1)  Case the current node is the winner: the node must be sure that the other nodes have detected the primary node failure and have started the election mechanism, otherwise the broadcast_call will have no effect.
+			erlang:send_after(?VICTORY_TIMEOUT, bully_algorithm, {notify_victory});
+
 		true ->
-			% (1) Case there are node with id greater than mine
-			Leader_Node = broadcast_election(Higher_id_list),
-			if
-				Leader_Node == no_answer ->
-				 % (2) Case no nodes reply with an answer to the election message
-				 broadcast_leader(Neighbours_list);
-				true ->
-					% (3) Node must wait for a victory message
-					erlang:send_after(?VICTORY_TIMEOUT, secondary_node, {check_leader})
-			end
+			erlang:send_after(?VICTORY_TIMEOUT, bully_algorithm, {check_leader})
 	end.
 
 handle_info(Info,{Neighbours,Leader}) ->
@@ -95,16 +94,33 @@ handle_info(Info,{Neighbours,Leader}) ->
 			if
 				Leader == no_leader ->
 					% (3) No victory message received after VICTORY_TIMEOUT milliseconds the election process must be restarted
-					start_election(Neighbours)
+					start_election(Neighbours);
+				true ->
+				
+					spawn(secondary_node, restart_link, [Leader])			
 			end;
-
+			
+		{notify_victory} ->
+		
+			broadcast_leader(Neighbours),
+			io:format("@@@@@BROADCAST HAS TERMINATED~n"), % DEBUG
+			spawn(primary_node, elect, []); 
+			%%spawn(primary_node, elect, [Neighbours]), 
+			
 		_Dummy ->
-			io:format("[dispatcher] WARNING: bad mex format in handle_info Format ~w ~n",[_Dummy])
+			io:format("[bully_algorithm] WARNING: bad mex format in handle_info Format ~w ~n",[_Dummy])
 	end,
 	{noreply,{Neighbours,Leader}}.
 
 
+handle_call(stop, From, {Neighbours,Leader}) ->
+    {stop, normal, shutdown_ok, {Neighbours,Leader}};
+	
+	
 handle_call(Request,From,{Neighbours,Leader}) ->
+
+	io:format("[bully_algorithm] CALL Request: ~w ~n",[Request]), %%DEBUG
+
 	case Request of
 		{Node,election} ->
 			if
@@ -113,38 +129,76 @@ handle_call(Request,From,{Neighbours,Leader}) ->
 					erlang:send_after(?VICTORY_TIMEOUT, secondary_node, {check_leader})
 			end,
 			{reply,answer,{Neighbours,Leader}};
+			
 		_Dummy ->
-			io:format("[dispatcher] WARNING: bad mex format in handle_info Format ~w ~n",[_Dummy])
+			io:format("[bully_algorithm] WARNING: bad mex format in handle_info Format ~w ~n",[_Dummy])
 	end.
 
 handle_cast(Request, {Neighbours,Leader}) ->
+
+	io:format("[bully_algorithm] CAST Request: ~w ~n",[Request]), %%DEBUG
 	case Request of
 		{Node,leader} ->
-			% (5) If the node receive a Vicory message it assumes that the sending node is the leader
+			% (5) If the node receive a Victory message it assumes that the sending node is the leader
+			%%spawn(secondary_node, restart_link, [Node]),
 			{noreply, {Neighbours, Node}};
+			
+		{Node,leader} ->
+			% (5) If the node receive a Victory message it assumes that the sending node is the leader
+			%%spawn(secondary_node, restart_link, [Node]),
+			{noreply, {Neighbours, Node}};
+		
 		_Dummy ->
-			io:format("[dispatcher] WARNING: bad mex format in handle_info Format ~w ~n",[_Dummy])
+			io:format("[bully_algorithm] WARNING: bad mex format in handle_info Format ~w ~n",[_Dummy])
 	end.
 
-get_neighbours_with_higher_id(Neighbours) ->
-	[Neighbour || Neighbour <- Neighbours, Neighbour > node()].
+get_neighbours_with_higher_id([]) ->
+	[];
+
+get_neighbours_with_higher_id(L) ->
+	
+	if 
+		is_list(L)-> 
+			Neighbours = L;
+		true ->
+			Neighbours = [L]
+		end,
+	
+	Nodes_id_str = [string:substr(atom_to_list(Node_name),5,2) || Node_name <- Neighbours],
+	Nodes_id_int = [list_to_integer(String_id) || String_id <- Nodes_id_str],
+	Current_Node_Id = list_to_integer(string:substr(atom_to_list(node()),5,2)),
+	io:format("@@@@@Current_Node_Id ~w~n",[Current_Node_Id]), % DEBUG
+	io:format("@@@@@Nodes_id_int ~w~n",[Nodes_id_int]), % DEBUG
+	Nodes_With_Higher_Id = [Node_id || Node_id <- Nodes_id_int, Node_id > Current_Node_Id],
+	io:format("@@@@@Nodes_With_Higher_Id ~w~n",[Nodes_With_Higher_Id]), % DEBUG
+	Nodes_With_Higher_Id.
 
 broadcast_leader([]) ->
 	ok;
 
-broadcast_leader([Current|Neighbours]) ->
+broadcast_leader([H|T]) ->
 	% If I'm the election winner I don't have to wait any reply from the nodes
-	gen_server:cast({secondary_node,Current},{node(),leader}),
-	broadcast_leader(Neighbours).
+	
+	io:format("[bully_algorithm] broadcast_leader on node: ~w ~n",[H]),
+		%%Reply =(catch gen_server:call({bully_algorithm,H},{node(),leader}, ?CALL_TIMEOUT)),
+	gen_server:cast({bully_algorithm,H},{node(),leader}),
+	broadcast_leader(T);
+	
+broadcast_leader(H) ->
+	% If I'm the election winner I don't have to wait any reply from the nodes
+	
+	io:format("[bully_algorithm] broadcast_leader on node: ~w ~n",[H]),
+	Reply =(catch gen_server:call({bully_algorithm,H},{node(),leader}, ?CALL_TIMEOUT)),
+	broadcast_leader([]).
 
 broadcast_election(Neighbours) ->
 	% multi_call(Nodes, Name, Request) makes a synchronous call to all gen_server processes locally
 	% registered as Name at the specified Nodes by first sending the request to every node and then
 	% waits for the replies. Where Replies is a list of {Node,Reply} and BadNodes is a list of node
 	% that either did not exist, or where the gen_server Name did not exist or did not reply.
-	{Replies,_} = gen_server:multi_call(Neighbours,secondary_node,{node(),election},?ELECTION_TIMEOUT),
+	{Replies,_} = gen_server:multi_call(Neighbours,bully_algorithm,{node(),election},?ELECTION_TIMEOUT),
 	% Gets the all the list of nodes that have replied with an answer message to the election
-	Multi_call_tuple = [Tuple || Tuple <- Replies, elem(2,Tuple) == answer],
+	Multi_call_tuple = [Tuple || Tuple <- Replies, element(2,Tuple) == answer],
 	Alive_nodes = [Node || {_,Node} <- Multi_call_tuple],
 	if
 		Alive_nodes == [] ->
